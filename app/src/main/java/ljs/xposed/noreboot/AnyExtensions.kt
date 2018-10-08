d504b086c266dd506719e1af49acdf3d51983248
ljs <ljs@ljs.ljs>
2018-10-08 23:53:49 +0800

    first commit

---
+++ app/src/main/java/ljs/xposed/noreboot/AnyExtensions.kt
@@ -0,0 +1,4 @@
+package ljs.xposed.noreboot
+
+val Any.Key_Configs: String
+    get() = "configs"
---
+++ app/src/main/java/ljs/xposed/noreboot/MainHook.kt
@@ -0,0 +1,14 @@
+package ljs.xposed.noreboot
+
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XSharedPreferences
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+class MainHook : IXposedHookLoadPackage {
+    var mXSharedPreferences = XSharedPreferences(BuildConfig.APPLICATION_ID)
+
+    override fun handleLoadPackage(loadPackageParam: XC_LoadPackage.LoadPackageParam) {
+        if (mXSharedPreferences.hasFileChanged()) {
+        }
+    }
+}
